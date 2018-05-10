1409a0db7149ecf00ab9740de53592c1b12c94b1
Dave Barber <dbarber1983@gmail.com>
2018-05-10 18:49:10 +0100

    Macminer V1

---
+++ src/3rdparty/libcpuid/CMakeFiles/cpuid.dir/DependInfo.cmake
@@ -0,0 +1,33 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/DavidBarber/webchain-miner/src/3rdparty/libcpuid/asm-bits.c" "/Users/DavidBarber/build-webchain-miner-Desktop_Qt_5_10_1_clang_64bit-Default/src/3rdparty/libcpuid/CMakeFiles/cpuid.dir/asm-bits.c.o"
+  "/Users/DavidBarber/webchain-miner/src/3rdparty/libcpuid/cpuid_main.c" "/Users/DavidBarber/build-webchain-miner-Desktop_Qt_5_10_1_clang_64bit-Default/src/3rdparty/libcpuid/CMakeFiles/cpuid.dir/cpuid_main.c.o"
+  "/Users/DavidBarber/webchain-miner/src/3rdparty/libcpuid/libcpuid_util.c" "/Users/DavidBarber/build-webchain-miner-Desktop_Qt_5_10_1_clang_64bit-Default/src/3rdparty/libcpuid/CMakeFiles/cpuid.dir/libcpuid_util.c.o"
+  "/Users/DavidBarber/webchain-miner/src/3rdparty/libcpuid/recog_amd.c" "/Users/DavidBarber/build-webchain-miner-Desktop_Qt_5_10_1_clang_64bit-Default/src/3rdparty/libcpuid/CMakeFiles/cpuid.dir/recog_amd.c.o"
+  "/Users/DavidBarber/webchain-miner/src/3rdparty/libcpuid/recog_intel.c" "/Users/DavidBarber/build-webchain-miner-Desktop_Qt_5_10_1_clang_64bit-Default/src/3rdparty/libcpuid/CMakeFiles/cpuid.dir/recog_intel.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "Clang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "NDEBUG"
+  "RAPIDJSON_SSE2"
+  "UNICODE"
+  "VERSION=\"0.4.0\""
+  "__STDC_FORMAT_MACROS"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
